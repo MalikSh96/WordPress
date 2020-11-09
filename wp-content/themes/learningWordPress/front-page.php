f372e9f087a27c2a8c59e95d6852e4c89ff23876
MalikSh96 <malik96sharfo@hotmail.com>
2020-11-09 16:36:54 +0100

    worked on using WP_Query

--- wp-content/themes/learningWordPress/front-page.php
+++ wp-content/themes/learningWordPress/front-page.php
@@ -13,9 +13,10 @@
       ?>
 
       <!--Home columns-->
-      <div class="home_columns clearfix">
+      <div class="home-columns clearfix">
         <!--One half-->
         <div class="one-half">
+          <h2>Latest about Shows</h2>
           <?php
           //Show posts loop begins here
           $showPosts = new WP_Query('cat=4&posts_per_page=2'); //<--class that ships with WordPress out of the box
@@ -27,8 +28,28 @@
               $showPosts->the_post();
               //output content however we please here
           ?>
-          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <!--Post item-->
+          <div class="post-item clearfix">
+            <!--Post thumbnail-->
+            <div class="square-thumbnail">
+              <a href="<?php the_permalink(); ?>">
+                <?php
+										if (has_post_thumbnail()) {
+											the_post_thumbnail('square-thumbnail');
+										}
+                    else {
+                ?>
+											<img src="<?php echo get_template_directory_uri(); ?>/images/BlackClover1.jpg">
+								<?php
+                    }
+                ?>
+              </a>
+						  </div>
+            <!-- /post-thumbnail -->
+          <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <span class="subtle-date"><?php the_time('n/j/Y'); ?></span></h4>
           <?php the_excerpt(); ?>
+          </div>
+          <!-- /post-item -->
           <?php
             endwhile;
 
@@ -40,22 +61,45 @@
           as it is relinquishing control of the global post variable, which then gives control
           back to WordPress and does not anylonger disrupt the url based queries, wp_reset_postdata resets the data*/
           ?>
+          <span class="horiz-center"><a href="<?php echo get_category_link(7); ?>" class="btn-a">View all Shows Posts</a></span>
         </div>
         <!--one half-->
+
         <div class="one-half last">
+          <h2>Latest about My Shows</h2>
           <?php
-          //MyShows posts loop begins here
-          $myShowPosts = new WP_Query('cat=7&posts_per_page=2'); //<--class that ships with WordPress out of the box
+          //Show posts loop begins here
+          $showPosts = new WP_Query('cat=7&posts_per_page=2'); //<--class that ships with WordPress out of the box
           //$showPosts = new WP_Query('cat=4&posts_per_page=2&orderby=title&order=ASC');
           //WP_Query returns an object which contains posts from the Shows category that we store in $showPosts
 
-          if($myShowPosts->have_posts()) :
-            while($myShowPosts->have_posts()) :
-              $myShowPosts->the_post();
+          if($showPosts->have_posts()) :
+            while($showPosts->have_posts()) :
+              $showPosts->the_post();
               //output content however we please here
           ?>
-          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <!--Post item-->
+          <div class="post-item clearfix">
+            <!--Post thumbnail-->
+            <div class="square-thumbnail">
+              <a href="<?php the_permalink(); ?>">
+                <?php
+										if (has_post_thumbnail()) {
+											the_post_thumbnail('square-thumbnail');
+										}
+                    else {
+                ?>
+											<img src="<?php echo get_template_directory_uri(); ?>/images/AkatsukiNoYona1.jpg">
+								<?php
+                    }
+                ?>
+              </a>
+						  </div>
+            <!-- /post-thumbnail -->
+          <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <span class="subtle-date"><?php the_time('n/j/Y'); ?></span></h4>
           <?php the_excerpt(); ?>
+          </div>
+          <!-- /post-item -->
           <?php
             endwhile;
 
@@ -67,6 +111,7 @@
           as it is relinquishing control of the global post variable, which then gives control
           back to WordPress and does not anylonger disrupt the url based queries, wp_reset_postdata resets the data*/
           ?>
+          <span class="horiz-center"><a href="<?php echo get_category_link(7); ?>" class="btn-a">View all My Shows Posts</a></span>
         </div>
       </div>
     </div>
